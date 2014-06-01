193179762dad69cbfd5f9e9421677af5e7a2ba8c
298410233 <ji-ye@Ji-Yeteki-MacBook-Pro.local>
2014-06-01 15:03:03 +0800

    298410233_FinalHW

---
+++ Final_298410233/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Final_298410233
+//
+//  Created by 298410233 on 14/6/1.
+//  Copyright (c) 2014年 298410233. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *Label;
+- (IBAction)Button1:(id)sender;
+- (IBAction)Button2:(id)sender;
+
+@end
---
+++ Final_298410233/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  Final_298410233
+//
+//  Created by 298410233 on 14/6/1.
+//  Copyright (c) 2014年 298410233. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)Button1:(id)sender
+{
+    self.Label.text=@"298410233";
+}
+
+- (IBAction)Button2:(id)sender
+{
+    self.Label.text=@"曾斐筠";
+}
+@end
